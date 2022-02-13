ea255d17e75ad65feca2b82c41caac95f0b6b62f
joaquinboto <joaquin.boto@hotmail.com>
2022-02-13 18:53:03 -0300

    loresolvi

--- js/style.js
+++ js/style.js
@@ -1,4 +1,3 @@
-
 const carrito = document.querySelector(".add-tocart")
 let contenedor = document.querySelector(".div-contenedor")
 
@@ -12,11 +11,24 @@ function presionar(e) {
     if (e.target.classList.contains('btn-cart')) {
         console.log(e.target);
     }
+    sendFuncion()
 }
 
+function sendFuncion() {
+    const infoProducto = document.createElement("div")
+    infoProducto.innerHTML = ` <div class="dv-img-producto">
+    <img src="./assets/conjuntobasico.jpeg" alt="">
+    </div>
+    <div class="product-item-details">
+      <h5>Conjunto Basico</h5>
+      <h6>$8000</h6>
+      <button class="btn-cart">ADD TO CART</button>
+    </div>`
+    carrito.appendChild(infoProducto)
+}
 
-
-
+const hola = document.createElement ("div")
+hola.innerHTML = `hola`
 
 
 
