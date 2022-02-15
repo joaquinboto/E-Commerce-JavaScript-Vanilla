3ffd31399df8ee5ab1f6770d69eca0f7a206ddcf
joaquinboto <joaquin.boto@hotmail.com>
2022-02-15 16:56:35 -0300

    desafioDOM

--- js/style.js
+++ js/style.js
@@ -1,14 +1,21 @@
-const addToCard = document.querySelectorAll(".btn-cart")
-console.log(addToCard);
-
-addToCard.forEach (addToCardBtn => {
-    addToCardBtn.addEventListener("click", addCard)
-})
-
+const producto2 = [
+  { nombre: "Remera",
+precio: 3000,
+imagen: `src="./assets/zapatillas.jpeg"`}
+]
 
 
-function addCard (event) {
-    const boton = event.target
-    console.log(boton);
 
+
+const carritoDeCompras = document.querySelector(".carrito")
+producto2.forEach(element => {
+  const productos = document.createElement ("div")
+  productos.className = "carrito"
+  productos.innerHTML= `<img  class="imagenCarrito" ${element.imagen} alt="">
+  <strong>${element.nombre}</strong>
+  <h6 class="precioCarrito">Precio: $${element.precio}</h6>
+  <button class="btn-delete">X</button>`
+  carritoDeCompras.appendChild(productos)
 }
+);
+
