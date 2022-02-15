11bf7cb172cbe0ebc946f9153555cfd90a902e8b
joaquinboto <joaquin.boto@hotmail.com>
2022-02-15 17:13:38 -0300

    arregle

--- js/style.js
+++ js/style.js
@@ -1,4 +1,4 @@
-const producto2 = [
+const producto3 = [
   { nombre: "Remera",
 precio: 3000,
 imagen: `src="./assets/zapatillas.jpeg"`}
@@ -8,14 +8,20 @@ imagen: `src="./assets/zapatillas.jpeg"`}
 
 
 const carritoDeCompras = document.querySelector(".carrito")
-producto2.forEach(element => {
-  const productos = document.createElement ("div")
-  productos.className = "carrito"
-  productos.innerHTML= `<img  class="imagenCarrito" ${element.imagen} alt="">
-  <strong>${element.nombre}</strong>
-  <h6 class="precioCarrito">Precio: $${element.precio}</h6>
-  <button class="btn-delete">X</button>`
-  carritoDeCompras.appendChild(productos)
-}
-);
 
+const agregarCarrito = () => {
+  
+  producto3.forEach(element => {
+    const productos = document.createElement ("div")
+    productos.className = "carrito"
+    productos.innerHTML= `<img  class="imagenCarrito" ${element.imagen} alt="">
+    <strong>${element.nombre}</strong>
+    <h6 class="precioCarrito">Precio: $${element.precio}</h6>
+    <button class="btn-delete">X</button>`
+    carritoDeCompras.appendChild(productos)
+  }
+  );
+}
+
+const boton = document.querySelector(".btn-cart3")
+boton.addEventListener("click",agregarCarrito)
