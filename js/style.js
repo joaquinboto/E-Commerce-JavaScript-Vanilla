70841fb3b1ea17f3c5a54dc32336e69140f5b9ca
joaquinboto <joaquin.boto@hotmail.com>
2022-02-18 16:12:20 -0300

    hasta aca llegue

--- js/style.js
+++ js/style.js
@@ -1,27 +1,47 @@
-const producto3 = [
-  { nombre: "Remera",
-precio: 3000,
-imagen: `src="./assets/zapatillas.jpeg"`}
-]
+let allContainerCart = document.querySelector('.div-contenedor');
+let containerBuyCart = document.querySelector('.carrito');
 
 
-
-
-const carritoDeCompras = document.querySelector(".carrito")
-
-const agregarCarrito = () => {
-  
-  producto3.forEach(element => {
-    const productos = document.createElement ("div")
-    productos.className = "carrito"
-    productos.innerHTML= `<img  class="imagenCarrito" ${element.imagen} alt="">
-    <strong>${element.nombre}</strong>
-    <h6 class="precioCarrito">Precio: $${element.precio}</h6>
-    <button class="btn-delete">X</button>`
-    carritoDeCompras.appendChild(productos)
-  }
-  );
+//Array
+const arrayProductos = []
+const arrayProductosCopia = Object.assign([] , arrayProductos )
+//Funciones
+loadEventos()
+function loadEventos() {
+    allContainerCart.addEventListener("click",addProducto)
 }
 
-const boton = document.querySelector(".btn-cart3")
-boton.addEventListener("click",agregarCarrito)
+function addProducto(e) {
+    e.preventDefault()
+    if (e.target.classList.contains("btn-add-cart")) {
+        const selectProduct = e.target.parentElement
+        readTheContent(selectProduct)
+    }
+}
+
+function readTheContent(producto) {
+    const arrayProductos = {
+        image: producto.querySelector("div img").src,
+        title: producto.querySelector("div h5").textContent,
+        price: producto.querySelector("div h6").textContent,
+        id: producto.querySelector("div a").getAttribute("data-id"),
+        cantidad: 1,
+    }
+    const arrayProductosCopia = Object.assign([] , arrayProductos )
+    console.log(arrayProductosCopia);
+    loadHTML()
+}
+
+function loadHTML() {
+    arrayProductosCopia.forEach(producto => {
+        console.log(producto);
+        const row = document.createElement("div");
+        row.innerHTML = `
+        <img class="imagen-producto" ${image.product} alt="">
+                    <h5>${title.producto}</h5>
+                    <h6 class="precio-producto">${price.producto}</h6>
+                    <a href="" data-id="${id.producto}" class="btn-add-cart">ADD TO CART</a>
+                    <p>${cantidad}</p>`
+        containerBuyCart.appendChild(row);
+    });
+}
