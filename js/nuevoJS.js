d5fbf7bc232a664d821ca4379a3fe59fd4fb77fa
joaquinboto <joaquin.boto@hotmail.com>
2022-02-27 19:59:37 -0300

    listo

--- js/nuevoJS.js
+++ js/nuevoJS.js
@@ -4,7 +4,7 @@ let precioProducto = document.querySelector(".precio-producto")
 let btnProducto = document.querySelectorAll(".btnProducto")
 let imagenProducto = document.querySelector(".imagen-producto")
 let divCarrito = document.querySelector(".carrito")
-
+let arreglo = []
 
 //------------------EVENTOS------------------
 btnProducto.forEach(element => {
@@ -14,14 +14,40 @@ btnProducto.forEach(element => {
 function eventoBoton (evento) {
     let boton = evento.target
     const productos = boton.closest(".producto1")
-    const nombre = productos.querySelector(".producto-title").textContent
-    const imagen = productos.querySelector(".imagen-producto").src
-    const precio = productos.querySelector(".precio-producto").textContent
-    updateCarrito (nombre, imagen , precio) //buscando datos de producto
+    buscarObjeto(productos)
+}
+
+function buscarObjeto (productos) {
+  const infoProduct = {
+    nombre : productos.querySelector(".producto-title").textContent,
+    imagen : productos.querySelector(".imagen-producto").src,
+    precio : productos.querySelector(".precio-producto").textContent,
+    value : productos.querySelector(".btnProducto").value
+  }
+
+  const existe = arreglo.some ((product) => product.nombre === infoProduct.nombre )
+  if (existe) {
+    const nuevoArreglo = arreglo.map ((e) => {
+      if (e.nombre === infoProduct.nombre) {
+        e.value++
+        return e
+      }
+      else {
+        return e
+      }
+    })
+    arreglo = [... nuevoArreglo]
+  }else {
+    arreglo = [...arreglo , infoProduct]
+  }
+  updateCarrito (arreglo) //buscando datos de producto
 }
 
 //--------------PINTAR PRODUCTOS-----------------
-function updateCarrito (nombre , imagen, precio){
+function updateCarrito (arreglo){
+  clear()
+  arreglo.forEach(productosVarios => {
+    const {nombre , imagen , precio , value} = productosVarios
     const row = document.createElement("div");
     row.className = "producto2"
     row.innerHTML = 
@@ -40,13 +66,12 @@ function updateCarrito (nombre , imagen, precio){
 
           <!--DIV INPUT Y BOTON-->
                 <div class="mockup3">
-                    <input class="inputControl mx-3" style="width:40px" type="number" value="1" >
+                    <input class="inputControl mx-3" style="width:40px" type="number" value="${value}" >
                     <a href="#" class="btn btn-danger" name="delete"> Delete <a/>
                 </div>
   `
     divCarrito.appendChild(row)
-    
-    
+
     //------------EVENTO BORRAR PRODUCTO Y PRECIO---------------------
     row.querySelector(".btn").addEventListener("click", (e) => {
         const botonDelete = e.target
@@ -59,11 +84,13 @@ function updateCarrito (nombre , imagen, precio){
         input.value <= 0 ? (input.value = 1) : null;
         sumarProducto()//SUMANDO INPUT
       })
-
-    
+  })
     sumarProducto()//---------SUMAR PRECIO----------
 }
 
+function clear() {
+  divCarrito.innerHTML = ''
+}
 //-------------SUMA DE PRODUCTOS-----------
 function sumarProducto() {
     let total = 0 
